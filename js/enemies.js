3e95156a49949edbe85c8b24509897ff4f68dc5e
Marko <koron.marko@gmail.com>
2016-10-27 20:17:16 +0200

    Add files via upload
    added variables, functions

--- js/enemies.js
+++ js/enemies.js
@@ -1,16 +1,49 @@
 var Enemy = function(x, y){
-  //Variables here
-  this.pos = createVector(x, y); //Vector for holding position
+ /* Given the x and y coordinates creates an alien thats
+    moving left to right and viceversa.
+ */
 
-  //Functions here
-  this.update = function(){
-    // TODO
+  this.pos = createVector(x, y) //Vector for holding position
+  this.enemyWidth = 35; // the width of the alien
+  this.xVel = createVector(2, 0); // horizontal velocity
+  this.yVel = createVector(0, 15); // vertical velocity
+  this.chaceOfShooting = 5; // chance factor that the alien is going to shoot
+  this.dead = false; // flag for checking if the alien has been hit
+
+
+  var sprite = loadImage("invader1.png");
+
+  /*Shows the alien on the canvas*/
+  this.show = function() {
+      sprite.resize(this.enemyWidth, 0);
+      image(sprite, this.pos.x, this.pos.y);
   }
 
+  /*Moves the alien down by yVel and changes his direction*/
+  this.moveDown = function() {
+    this.xVel.mult(-1);
+    this.pos.add(this.yVel);
+  }
 
-  //Generates a bullet with a force,  downwards
+  /*Updates the position of the alien if it's not hit*/
+  this.update = function(){
+    if(this.dead) return;
+
+    this.pos.add(this.xVel);
+    if (this.pos.x  > width - this.enemyWidth || this.pos.x  < 0) {
+      this.moveDown();
+    }
+    this.shoot();
+  }
+  /*Generates a bullet with a force, downwards based on
+    a random value. */
   this.shoot = function(){
-
+    var a = Math.round(Math.random() * 1000);
+    console.log(a);
+    if (a < this.chaceOfShooting) {
+      //var bullet = new Bullet(-1);
+      console.log("Shoot!");
+    }
   }
 
   //Draws the enemy at current position
