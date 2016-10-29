ca1dde4ab51d2e02dc6a9e828ad500e4b9a52f90
Chaim Lando <chiptus@gmail.com>
2016-10-29 21:55:58 +0530

    removed es6

--- js/spaceShip.js
+++ js/spaceShip.js
@@ -1,46 +1,48 @@
 
 
-  //default parameters are to put the spaceShip in the center
-var SpaceShip = function (x = width / 2, y = height / 2) {
+//default parameters are to put the spaceShip in the center
+var SpaceShip = function (x, y) {
+  x = x || width / 2;
+  y = y || height / 2;
   //Variables here
   this.pos = createVector(x, y); //Vector for holding position
   this.vel = createVector(0, 0); // Vel changes position
   this.img = loadImage("images/spaceship.png");
-  let acc = createVector(0,0);
+  var acc = createVector(0, 0);
 
   //Functions here
   this.update = function update() {
     this.vel.add(acc)
     this.pos.add(this.vel);
-    acc.set(0,0);
+    acc.set(0, 0);
   }
 
-  function applyForce(force){
+  function applyForce(force) {
     acc.add(force);
   }
 
   this.move = function (moveCode) {
-    let forceVector = createVector(0,0);
+    var forceVector = createVector(0, 0);
     switch (moveCode) {
       //TODO make decisions here and apply Force towards that side in acc
       // acc.add(forceVector);
       case DOWN_ARROW:
-        forceVector.set(0,1);
+        forceVector.set(0, 1);
         break;
       case UP_ARROW:
-        forceVector.set(0,-1);        
+        forceVector.set(0, -1);
         break;
       case LEFT_ARROW:
-        forceVector.set(-1,0);
+        forceVector.set(-1, 0);
         break;
       case RIGHT_ARROW:
-        forceVector.set(1,0);
+        forceVector.set(1, 0);
         break;
     }
     applyForce(forceVector);
   }
 
- 
+
 
   //Generates a bullet with a force,  upwards
   this.shoot = function () {
@@ -56,7 +58,7 @@ var SpaceShip = function (x = width / 2, y = height / 2) {
 
 //Generate SpaceShip with constructor, give the x and y
 function placeSpaceShip() {
-  const spaceShip = new SpaceShip(canvas.width / 2, canvas.height - canvas.height / 5);
+  var spaceShip = new SpaceShip(canvas.width / 2, canvas.height - canvas.height / 5);
 
   return spaceShip;
 }
