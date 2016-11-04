6935c97c2143a4041bf3b3a959487367697304b0
a-sep <artur.sep@gmail.com>
2016-11-04 04:25:20 +0100

    deleted addAcc(), body of it added to :update()

--- js/bullets.js
+++ js/bullets.js
@@ -4,13 +4,7 @@ var Bullet = function(x, y, up) {
     this.pos = createVector(x, y);
 
     //Creates acceleration upwards or downwards, if up
-    this.acc = createVector(0, BulletForce * (up
-        ? -1
-        : + 1));
-
-    this.addAcc = function() {
-        this.pos.add(this.acc);
-    }
+    this.acc = createVector(0, BulletForce * (up ? -1 : +1));
 
     this.draw = function() {
         //Draw Bullet at position TODO
@@ -18,7 +12,7 @@ var Bullet = function(x, y, up) {
     }
 
     this.update = function() {
-        this.addAcc();
+        this.pos.add(this.acc);
         this.detectCollision();
     }
 
