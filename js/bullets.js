e1df863bff91c27b59b6e7461603ad181a79876a
Artur Sęp <artur.sep@gmail.com>
2016-11-06 13:04:23 +0100

    spaceship don't go out of the screen
    * draw bullets as rectangles
    
    * added bullets speed/acceleration
    
    * BulletForce moved to globalVars
    
    * BulletForce in globalVars
    
    * deleting bullets out of the screen
    
    * deleted addAcc(), body of it added to :update()
    
    * deleting unnecessary bullets (if out of the screen)
    
    * BulletForce moved to globalVars
    
    * added detect collision with spaceship
    
    * *added detect collision with enemies in bullets.js
    *modified enemies.shoot()
    *modified spaceShip.shoot()
    
    * movement of spaceship tuned up,
    added 'arrow down' as a break,

--- js/bullets.js
+++ js/bullets.js
@@ -5,7 +5,6 @@ var Bullet = function(x, y, up) {
     this.acc = createVector(0, BulletForce * (up ? -1 : +1));
 
     this.draw = function() {
-        //Draw Bullet at position TODO
         rect(this.pos.x, this.pos.y, 3, 7);
     }
 
@@ -18,7 +17,6 @@ var Bullet = function(x, y, up) {
         // Go through all game objects and check if bullet's x and y coordinates
         // are same any of the object, you can assume hitbox to be a rectangle
         // even if the enemies are looking funky
-        // TODO
 
         // if bullet leaves the screen delete it, otherwise amount of bullets slowdowns the game
         if (this.pos.y < 0 || this.pos.y > canvas.height) {
@@ -48,6 +46,5 @@ var Bullet = function(x, y, up) {
                 }
             }
         }
-
     }
 }
--- js/spaceShip.js
+++ js/spaceShip.js
@@ -1,64 +1,76 @@
-
-
 //default parameters are to put the spaceShip in the center
-var SpaceShip = function (x, y) {
-  x = x || width / 2;
-  y = y || height / 2;
-  //Variables here
-  this.pos = createVector(x, y); //Vector for holding position
-  this.vel = createVector(0, 0); // Vel changes position
-  this.img = loadImage("images/spaceship.png");
-  var acc = createVector(0, 0);
+var SpaceShip = function(x, y) {
+    x = x || width / 2;
+    y = y || height / 2;
+    //Variables here
+    this.pos = createVector(x, y); //Vector for holding position
+    this.vel = createVector(0, 0); // Vel changes position
+    this.img = loadImage("images/spaceship.png");
+    this.acc = createVector(0, 0);
 
-  //Functions here
-  this.update = function update() {
-    this.vel.add(acc)
-    this.pos.add(this.vel);
-    acc.set(0, 0);
-  }
-
-  function applyForce(force) {
-    acc.add(force);
-  }
-
-  this.move = function (moveCode) {
-    var forceVector = createVector(0, 0);
-    switch (moveCode) {
-      //TODO make decisions here and apply Force towards that side in acc
-      // acc.add(forceVector);
-      // case DOWN_ARROW:
-      //   forceVector.set(0, 1);
-      //   break;
-      // case UP_ARROW:
-      //   forceVector.set(0, -1);
-      //   break;
-      case LEFT_ARROW:
-        forceVector.set(-1, 0);
-        break;
-      case RIGHT_ARROW:
-        forceVector.set(1, 0);
-        break;
+    //Functions here
+    this.update = function update() {
+        this.pos.add(this.acc);
+        if (this.pos.x < 0) {
+            this.pos.x = 0.1;
+            this.acc.x = 0;
+        } else if (this.pos.x > canvas.width - this.img.width) {
+            this.pos.x = canvas.width - this.img.width - 0.1;
+            this.acc.x = 0;
+        } else {
+            this.pos.add(this.acc);
+        }
     }
-    applyForce(forceVector);
-  }
 
+    this.move = function(moveCode) {
+        var forceVector = createVector(0, 0);
+        switch (moveCode) {
+                //TODO make decisions here and apply Force towards that side in acc
+                // acc.add(forceVector);
+                // case DOWN_ARROW:
+                //   forceVector.set(0, 1);
+                //   break;
+                // case UP_ARROW:
+                //   forceVector.set(0, -1);
+                //   break;
+            case LEFT_ARROW:
+                console.log(this.acc.x);
+                if (this.acc.x > 0) {
+                    this.acc.x = 0;
+                    this.acc.x += -2;
+                } else if (this.acc.x > -10) {
+                    this.acc.x += -2;
+                }
+                break;
+            case RIGHT_ARROW:
+                console.log(this.acc.x);
+                if (this.acc.x < 0) {
+                    this.acc.x = 0;
+                    this.acc.x += 2;
+                } else if (this.acc.x < 10) {
+                    this.acc.x += 2;
+                }
+                break;
+            case DOWN_ARROW:
+                this.acc.x = 0;
+                break;
 
+        }
+    }
 
-  //Generates a bullet with a force,  upwards
-  this.shoot = function () {
-    var bullet = new Bullet(this.pos.x + this.img.width/2, this.pos.y, true);
-    gameObjects.push(bullet);
-  }
+    //Generates a bullet with a force,  upwards
+    this.shoot = function() {
+        var bullet = new Bullet(this.pos.x + this.img.width / 2, this.pos.y, true);
+        gameObjects.push(bullet);
+    }
 
-  this.draw = function () {
-    image(this.img, this.pos.x, this.pos.y, 100, 65);
-  }
+    this.draw = function() {
+        image(this.img, this.pos.x, this.pos.y, 100, 65);
+    }
 }
-
 
 //Generate SpaceShip with constructor, give the x and y
 function placeSpaceShip() {
-  var spaceShip = new SpaceShip(canvas.width / 2, canvas.height - canvas.height / 5);
-
-  return spaceShip;
+    var spaceShip = new SpaceShip(canvas.width / 2, canvas.height - canvas.height / 5);
+    return spaceShip;
 }
